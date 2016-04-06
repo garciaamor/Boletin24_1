15f74e48be6c09cd9a562ff33808f5aea27e3497
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-04-06 11:42:28 +0200

    boletin24

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,10 @@
+package boletin24;
+
+public class Boletin24 {
+
+    public static void main(String[] args) {
+        Frame24 boletin24=new Frame24();
+        boletin24.setVisible(true);
+    }
+    
+}
